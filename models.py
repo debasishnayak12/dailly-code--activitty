c14783d02b83ab1c96da0baaae598f6ea1d2474c
debasishnayak12 <137148823+debasishnayak12@users.noreply.github.com>
2024-11-13 17:40:19 +0530

    models file for the database

--- models.py
+++ models.py
@@ -1,16 +1,12 @@
-from sqlalchemy import Column, Integer, String ,DateTime
-from database import Base
-from datetime import datetime
+from django.db import models
 
+# Create your models here.
+from django.db import models
 
-class User(Base):
-    __tablename__ = "user"
-    
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(255), index=True)
-    email = Column(String(255), unique=True, index=True)
-    mobile = Column(Integer,index = True)
-    image = Column(String(255),index = True)
-    status = Column(Integer,index = True)
-    time = Column(DateTime,index=True ,default=datetime.utcnow)
+class user(models.Model):
+    id = models.IntegerField(auto_created=True,primary_key=True)
+    name = models.CharField(max_length=255)
+    mobile = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
