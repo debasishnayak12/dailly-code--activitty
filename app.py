47de5ca634993cd5a86f62d37507a33eb642889d
debasishnayak12 <137148823+debasishnayak12@users.noreply.github.com>
2024-12-01 17:43:38 +0530

    ajax api python file

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-from flask import Flask,render_template,request,jsonify,url_for
+from flask import Flask,render_template,request,jsonify
 import mysql.connector
-import os
+
 
 app =Flask(__name__)
 
-upload_folder  = 'static/uploads'
-app.config['upload_folder'] = upload_folder
 def db_connection():
     conn = mysql.connector.connect(
         host ="localhost",
@@ -19,35 +17,28 @@ def db_connection():
 def index():
     return render_template("index.html")
     
-if not os.path.exists(upload_folder):
-    os.makedirs(upload_folder)
-@app.route('/postimage',methods=["POST"])
-def postimage():
-    if 'image' not in request.files:
-        return jsonify({"Error":"NO file "}),400
-    file = request.files['image']
-    
-    if file.filename =='':
-        return jsonify({"error":"No file selected"}),400
-    
-    file_path = os.path.join(app.config['upload_folder'],file.filename)
-    file.save(file_path)
-    
-    image_url = url_for('static',filename = 'uploads/'+file.filename, _external = True)
-    
-    try:
-        conn = db_connection()
-        cur = conn.cursor(dictionary=True)
-        query = "insert into image_store(image) values(%s)"
-        val = (image_url,)
-        cur.execute(query,val)
-        conn.commit()
-        return jsonify({"Status":"True","message":"Insert data"}),200
+@app.route('/postdata',methods=["POST"])
+def postdata():
+    if request.method =="POST":
+        data = request.json
+        name = data.get('name')
+        phone = data.get('phone')
+        print(name,phone)
+        if not name or not phone  :
+            return jsonify({"Error":"anme and phone data required"})
+        try:
+            conn = db_connection()
+            cur = conn.cursor(dictionary=True)
+            query = "insert into customers(Name,Phone) values(%s,%s)"
+            val = (name,phone)
+            cur.execute(query,val)
+            conn.commit()
+            return jsonify({"Status":"True","message":"Insert data"})
 
-    except mysql.connector.Error as e:
-        return jsonify({"Status":"False","Message":f"{e}"})
-    # with open(f"{request.form.get('name')}.txt" ,"w") as f:
-    #     f.write("POST created ")
+        except mysql.connector.Error as e:
+            return jsonify({"Status":"False","Message":f"{e}"})
+        # with open(f"{request.form.get('name')}.txt" ,"w") as f:
+        #     f.write("POST created ")
         
 
         
