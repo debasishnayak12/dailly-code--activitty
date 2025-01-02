1e026b31b13aaa732295be508dc97a866f5841b9
debasishnayak12 <137148823+debasishnayak12@users.noreply.github.com>
2025-01-02 18:07:40 +0530

    models fiel for the django project

--- models.py
+++ models.py
@@ -1,12 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
-from django.db import models
-
-class user(models.Model):
-    id = models.IntegerField(auto_created=True,primary_key=True)
-    name = models.CharField(max_length=255)
-    mobile = models.CharField(max_length=255)
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)  # Links to the User model
+    age = models.IntegerField()
+    department = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.name
+        return self.user.username
